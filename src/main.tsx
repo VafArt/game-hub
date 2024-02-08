d2b8a485b32e844e1de752cd74a36d4cb4bc2e4a
Vafin Artur <ashot_one_shot@rambler.ru>
2024-02-08 21:56:43 +0300

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from "./theme.ts";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <HashRouter>
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </React.StrictMode>
+  </HashRouter>
 );
