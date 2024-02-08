f898d877d49f2e33464e434c27d942b01d950807
Vafin Artur <ashot_one_shot@rambler.ru>
2024-02-08 20:22:36 +0300

    Ship genres and platforms with app

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,6 +1,5 @@
 import bullsEye from "../assets/bulls-eye.webp";
 import thumbUp from "../assets/thump-up.webp";
-import meh from "../assets/meh.webp";
 import { Image, ImageProps } from "@chakra-ui/react";
 
 interface Props {
@@ -8,15 +7,14 @@ interface Props {
 }
 
 const Emoji = ({ rating }: Props) => {
-  if (rating < 3) return null;
+  if (rating < 4) return null;
 
   const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: "meh", boxSize: "25px" },
-    4: { src: thumbUp, alt: "recommended", boxSize: "25px" },
+    4: { src: thumbUp, alt: "recommended", boxSize: "22px" },
     5: { src: bullsEye, alt: "exceptionsl", boxSize: "25px" },
   };
 
-  return <Image {...emojiMap[rating]} marginTop={1} />;
+  return <Image {...emojiMap[rating]} marginTop={2} />;
 };
 
 export default Emoji;
